536f10c864ab5fd6818bf476a8646e7af49dec20
Kai <kizaruhandsome10@gmail.com>
2026-09-12 09:24:49 +0800

    BeastHub.lua

--- BeastHub.lua
+++ BeastHub.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("KaiHub X BeastHub is current disabled for now. I will try to update it but soon.\n\nSorry my dear.\n\nThank you for using this script!💗")
+game.Players.LocalPlayer:Kick("KaiHub X BeastHub is current disabled for now. I will try to update it but soon.\nSorry my dear.\nThank you for using this script!💗")
